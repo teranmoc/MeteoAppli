af21f7bf45041e2c9a3eb8a9467deb78c441e618
jeremie <jeremie.decome12@gmail.com>
2016-11-02 17:41:43 +0100

    Question 2 ok

---
+++ app/src/main/java/com/example/jrmie/meteoappli/CityManager.java
@@ -0,0 +1,7 @@
+package com.example.jrmie.meteoappli;
+
+/**
+ * Created by Jérémie on 12/10/2016.
+ */
+public class CityManager {
+}
---
+++ app/src/main/java/com/example/jrmie/meteoappli/SettingActivity.java
@@ -0,0 +1,7 @@
+package com.example.jrmie.meteoappli;
+
+/**
+ * Created by Jérémie on 26/10/2016.
+ */
+public class SettingActivity {
+}
